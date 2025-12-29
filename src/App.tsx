f9a3987bced82a76e65897d29e808ee6670926f6
dallin-windows-hp <dallin-windows-hp>
2025-12-29 12:17:44 -0700

    Basic html/css layout

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,22 @@ function App() {
 
   return (
     <>
+        <div className="left menu">
+            <div className="library pane">
+                Add components
+            </div>
+            <div className="contents pane">
+                Existing components
+            </div>
+        </div>
+        <div className="right menu">
+            Properties
+        </div>
+        <div className="editor">
+            <div className="screen pane">
+
+            </div>
+        </div>
     </>
   )
 }
